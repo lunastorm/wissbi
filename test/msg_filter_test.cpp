9e17fa4b013388cc017e27cd8cda42483804b2a7
Scott Wang (lunastorm) <lunastorm@lunastorm.tw>
2012-11-28 21:28:46 +0800

    add filter loop test

--- test/msg_filter_test.cpp
+++ test/msg_filter_test.cpp
@@ -6,6 +6,7 @@ using wissbi::MsgBuf;
 using wissbi::MsgFilter;
 using ::testing::_;
 using ::testing::Return;
+using namespace std;
 
 class MockInputPolicy {
     public:
@@ -33,5 +34,22 @@ TEST_F(MsgFilterTest, ReadFail) {
     EXPECT_CALL(static_cast<MockInputPolicy&>(filter), Get(_)).WillOnce(Return(false));
     EXPECT_CALL(static_cast<MockOutputPolicy&>(filter), Put(_)).Times(0);
     EXPECT_FALSE(filter.Filter());
+}
+
+TEST_F(MsgFilterTest, Filter) {
+    MsgFilter<MockInputPolicy, MockOutputPolicy> filter;
+    MsgBuf *buf_ptr;
+    EXPECT_CALL(static_cast<MockInputPolicy&>(filter), Get(_)).WillOnce(Return(true));
+    EXPECT_CALL(static_cast<MockOutputPolicy&>(filter), Put(_)).WillOnce(Return(true));
+    filter.Filter();
+}
+
+TEST_F(MsgFilterTest, FilterLoop) {
+    MsgFilter<MockInputPolicy, MockOutputPolicy> filter;
+    MsgBuf *buf_ptr;
+    EXPECT_CALL(static_cast<MockInputPolicy&>(filter), Get(_)).Times(4)
+        .WillOnce(Return(true)).WillOnce(Return(true)).WillOnce(Return(true)).WillRepeatedly(Return(false));
+    EXPECT_CALL(static_cast<MockOutputPolicy&>(filter), Put(_)).Times(3).WillRepeatedly(Return(true));
+    filter.FilterLoop();
 }
 
