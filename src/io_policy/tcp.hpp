d25939f24710a9c751290a07bdfa5a9c58458da6
Scott Wang (lunastorm) <lunastorm@lunastorm.tw>
2012-11-29 23:50:55 +0800

    add TCP policy

---
+++ src/io_policy/tcp.hpp
@@ -0,0 +1,87 @@
+#ifndef WISSBI_IO_POLICY_TCP_HPP_
+#define WISSBI_IO_POLICY_TCP_HPP_
+
+#include "msg_buf.hpp"
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+namespace wissbi {
+namespace io_policy {
+
+class TCP {
+    public:
+    TCP() {
+        sock_ = socket(AF_INET, SOCK_STREAM, 0);
+    }
+
+    ~TCP() {
+        close(sock_);
+    }
+
+    bool Put(const MsgBuf &msg) {
+        if(msg.len <= 0) {
+            return false;
+        }
+        uint32_t len_header = htonl(msg.len);
+        return WriteAll(sock_, reinterpret_cast<char*>(&len_header), 4) && WriteAll(sock_, msg.buf, msg.len);
+    }
+
+    bool Get(MsgBuf *msg_ptr) {
+        uint32_t len_header;
+        if(4 != ReadAll(sock_, reinterpret_cast<char*>(&len_header), 4)) {
+            msg_ptr->len = 0;
+            return false;
+        }
+        msg_ptr->len = ntohl(len_header);
+        if(msg_ptr->len != ReadAll(sock_, msg_ptr->buf, msg_ptr->len)) {
+            msg_ptr->len = 0;
+            return false;
+        }
+        return true;
+    }
+
+    void Connect(const sockaddr *addr) {
+        if(-1 == connect(sock_, addr, sizeof(*addr))) {
+            throw "cannot connect";
+        }
+    }
+
+    void AttachConnectedSock(int sock) {
+        close(sock_);
+        sock_ = sock;
+    }
+
+    protected:
+    static ssize_t ReadAll(int blocking_fd, char* buf, size_t size) {
+        size_t size_read = 0;
+        while(size_read < size) {
+            ssize_t n = read(blocking_fd, buf + size_read, size - size_read);
+            if(n <= 0) {
+                return n;
+            }
+            size_read += n;
+        }
+        return size_read;
+    }
+
+    static bool WriteAll(int blocking_fd, const char* buf, size_t size) {
+        size_t size_write = 0;
+        while(size_write < size) {
+            ssize_t n = write(blocking_fd, buf + size_write, size - size_write);
+            if(n == -1) {
+                std::cerr<<"write error: " << strerror(errno) << std::endl;
+                return false;
+            }
+            size_write += n;
+        }
+        return true;
+    }
+
+    private:
+    int sock_;
+};
+
+}
+}
+
+#endif  // WISSBI_IO_POLICY_TCP_HPP_
---
+++ test/tcp_io_policy_test.cpp
@@ -0,0 +1,110 @@
+#include "gtest/gtest.h"
+#include "io_policy/tcp.hpp"
+#include <sys/time.h>
+#include <thread>
+
+using wissbi::MsgBuf;
+using wissbi::io_policy::TCP;
+using std::string;
+using std::thread;
+
+class TCPIOPolicyTest : public ::testing::Test, public TCP {
+    protected:
+    virtual void SetUp() {
+        sock_ = -1;
+        serv_ = socket(AF_INET, SOCK_STREAM, 0);
+        ASSERT_NE(-1, serv_);
+
+        serv_addr_.sin_family = AF_INET;
+        serv_addr_.sin_port = htons(0);
+        serv_addr_.sin_addr.s_addr = inet_addr("127.0.0.1");
+        memset(&(serv_addr_.sin_zero), '\0', 8);
+
+        ASSERT_NE(-1, bind(serv_, reinterpret_cast<sockaddr*>(&serv_addr_), sizeof(serv_addr_)));
+        ASSERT_NE(-1, listen(serv_, 10));
+        socklen_t len;
+        ASSERT_NE(-1, getsockname(serv_, reinterpret_cast<sockaddr*>(&serv_addr_), &len));
+
+        thread([this]{
+            sockaddr_in client_addr;
+            socklen_t len;
+            sock_ = accept(serv_, reinterpret_cast<sockaddr*>(&client_addr), &len);
+            struct timeval tv;
+            tv.tv_sec = 1;
+            tv.tv_usec = 0;
+            setsockopt(sock_, SOL_SOCKET, SO_RCVTIMEO, &tv, sizeof(tv));
+        }).detach();
+    }
+
+    virtual void TearDown() {
+        close(serv_);
+        close(sock_);
+    }
+
+    int serv_;
+    int sock_;
+    sockaddr_in serv_addr_;
+};
+
+TEST_F(TCPIOPolicyTest, PutEmpty) {
+    MsgBuf msg_buf;
+    msg_buf.len = 0;
+    Connect(reinterpret_cast<sockaddr*>(&serv_addr_));
+    EXPECT_FALSE(Put(msg_buf));
+}
+
+TEST_F(TCPIOPolicyTest, PutOne) {
+    string msg("Hello World");
+    MsgBuf msg_buf;
+    strncpy(msg_buf.buf, msg.c_str(), msg.length());
+    msg_buf.len = msg.length();
+
+    MsgBuf new_buf;
+    Connect(reinterpret_cast<sockaddr*>(&serv_addr_));
+    while(sock_ == -1) {}
+    EXPECT_TRUE(Put(msg_buf));
+
+    uint32_t len_header;
+    ReadAll(sock_, reinterpret_cast<char*>(&len_header), 4);
+    new_buf.len = ntohl(len_header);
+    EXPECT_EQ(msg.length(), new_buf.len);
+    ReadAll(sock_, new_buf.buf, new_buf.len);
+    EXPECT_STREQ(msg.c_str(), string(new_buf.buf, new_buf.len).c_str());
+}
+
+TEST_F(TCPIOPolicyTest, GetOne) {
+    string msg("Hello World");
+    MsgBuf msg_buf;
+    strncpy(msg_buf.buf, msg.c_str(), msg.length());
+    msg_buf.len = msg.length();
+
+    MsgBuf new_buf;
+    Connect(reinterpret_cast<sockaddr*>(&serv_addr_));
+    while(sock_ == -1) {}
+
+    uint32_t len_header = htonl(msg_buf.len);
+    EXPECT_TRUE(WriteAll(sock_, reinterpret_cast<char*>(&len_header), 4));
+    EXPECT_TRUE(WriteAll(sock_, msg_buf.buf, msg_buf.len));
+    EXPECT_TRUE(Get(&new_buf));
+    EXPECT_STREQ(msg.c_str(), string(new_buf.buf, new_buf.len).c_str());
+}
+
+TEST_F(TCPIOPolicyTest, AttachConnectedSock) {
+    string msg("Hello World");
+    MsgBuf msg_buf;
+    strncpy(msg_buf.buf, msg.c_str(), msg.length());
+    msg_buf.len = msg.length();
+
+    MsgBuf new_buf;
+    int new_sock = socket(AF_INET, SOCK_STREAM, 0);
+    ASSERT_NE(-1, connect(new_sock, reinterpret_cast<sockaddr*>(&serv_addr_), sizeof(serv_addr_)));
+    while(sock_ == -1) {}
+    AttachConnectedSock(new_sock);
+
+    uint32_t len_header = htonl(msg_buf.len);
+    EXPECT_TRUE(WriteAll(sock_, reinterpret_cast<char*>(&len_header), 4));
+    EXPECT_TRUE(WriteAll(sock_, msg_buf.buf, msg_buf.len));
+    EXPECT_TRUE(Get(&new_buf));
+    EXPECT_STREQ(msg.c_str(), string(new_buf.buf, new_buf.len).c_str());
+}
+
