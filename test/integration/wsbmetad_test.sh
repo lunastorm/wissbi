66fda66db02e5a8f129abc3e7ac03e2729db92b1
lunastorm <lunastorm@zillians.com>
2013-02-18 14:27:13 +0800

    fix wsbmetad test

--- test/integration/wsbmetad_test.sh
+++ test/integration/wsbmetad_test.sh
@@ -3,16 +3,16 @@
 testDeadEntryCleanup()
 {
     mkdir -p $TMP_META_DIR/sub/foo
-    touch -d @$((`date +%s`-120)) $TMP_META_DIR/sub/foo/dead_entry
-    touch $TMP_META_DIR/sub/foo/live_entry
+    touch -d @$((`date +%s`-120)) $TMP_META_DIR/sub/foo/1.1.1.1:12345,dead.entry
+    touch $TMP_META_DIR/sub/foo/1.1.1.1:1234,live.entry
 
     env WISSBI_META_DIR=$TMP_META_DIR $PROJECT_ROOT/scripts/wsbmetad.sh &
     WSBMETAD_PID=$!
     sleep 1
     kill $WSBMETAD_PID
 
-    assertFalse "dead entry not removed" "ls $TMP_META_DIR/sub/foo/dead_entry"
-    assertTrue "live entry wrongly removed" "ls $TMP_META_DIR/sub/foo/live_entry"
+    assertFalse "dead entry not removed" "ls $TMP_META_DIR/sub/foo/1.1.1.1:12345,dead.entry"
+    assertTrue "live entry wrongly removed" "ls $TMP_META_DIR/sub/foo/1.1.1.1:1234,live.entry"
 }
 
 oneTimeSetUp()
