d4d69a09c5e46f73daf4676d9d14d46ec3cd78c3
lunastorm <lunastorm@zillians.com>
2013-02-18 12:41:05 +0800

    add wsbmetad.sh and test

---
+++ scripts/wsbmetad.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+set -o nounset
+set -o errexit
+echo $WISSBI_META_DIR
+
+: ${WISSBI_META_DIR:="/var/lib/wissbi"}
+
+while true
+do
+    for ENTRY in `find $WISSBI_META_DIR/sub`
+    do
+        ATIME=`stat -c "%X" $ENTRY || true`
+        if [ "$((`date +%s` - ATIME))" -gt 60 ]
+        then
+            rm $ENTRY
+        fi
+    done
+    sleep 1
+done
+
