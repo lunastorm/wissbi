f083dc6e5c589af5702c3a15e2ecfeb7e7c221b7
lunastorm <lunastorm@zillians.com>
2013-02-25 19:23:23 +0800

    fix wsbmetad backup and test: only restore when there is nothing

--- scripts/wsbmetad.sh
+++ scripts/wsbmetad.sh
@@ -6,10 +6,15 @@ set -o errexit
 : ${WISSBI_META_DIR:="/var/lib/wissbi"}
 : ${WISSBI_BACKUP_DIR:="/var/lib/wissbi-backup"}
 
-rsync -rdlpAog $WISSBI_BACKUP_DIR/* $WISSBI_META_DIR
+if [ -z "`ls $WISSBI_META_DIR`" ]
+then
+    logger -s -i -t wsbmetad "restoring wissbi metadata from backup"
+    rsync -rdlpAog --delete $WISSBI_BACKUP_DIR/* $WISSBI_META_DIR
+fi
+
 while true
 do
-    rsync -rdlpAog $WISSBI_META_DIR/* $WISSBI_BACKUP_DIR
+    rsync -rdlpAog --delete $WISSBI_META_DIR/* $WISSBI_BACKUP_DIR
     for ENTRY in `find $WISSBI_META_DIR/sub | grep ":"`
     do
         ATIME=`stat -c "%X" $ENTRY || true`
--- test/integration/wsbmetad_test.sh
+++ test/integration/wsbmetad_test.sh
@@ -6,7 +6,7 @@ testDeadEntryCleanup()
     touch -d @$((`date +%s`-120)) $TMP_META_DIR/sub/foo/1.1.1.1:12345,dead.entry
     touch $TMP_META_DIR/sub/foo/1.1.1.1:1234,live.entry
 
-    env WISSBI_META_DIR=$TMP_META_DIR $PROJECT_ROOT/scripts/wsbmetad.sh &
+    env WISSBI_META_DIR=$TMP_META_DIR WISSBI_BACKUP_DIR=$TMP_BACKUP_DIR $PROJECT_ROOT/scripts/wsbmetad.sh &
     WSBMETAD_PID=$!
     sleep 1
     kill $WSBMETAD_PID
@@ -28,12 +28,14 @@ oneTimeTearDown()
 setUp()
 {
     TMP_META_DIR=`mktemp -d`
+    TMP_BACKUP_DIR=`mktemp -d`
     mkdir -p $TMP_META_DIR/sub
 }
 
 tearDown()
 {
     rm -rf $TMP_META_DIR
+    rm -rf $TMP_BACKUP_DIR
 }
 
 . ../../3rd_party/shunit2/src/shell/shunit2
