f4abb1ea9070b26d5aee67efe5004da5fb63e964
lunastorm <lunastorm@zillians.com>
2013-02-25 21:41:03 +0800

    add source only and sink only filter template test

--- test/integration/filter_template_test.sh
+++ test/integration/filter_template_test.sh
@@ -1,5 +1,35 @@
 #!/bin/bash
 
+testNoSink()
+{
+    cat > $TMP_META_DIR/filterd <<EOF
+#!/bin/sh
+
+WISSBI_FILTER_COUNT="1"
+WISSBI_FILTER_CMD="sed --unbuffered -e 's/foo/bar/'"
+WISSBI_FILTER_SOURCE="foo.in"
+WISSBI_FILTER_SINK=""
+WISSBI_FILTER_LOG_PREFIX="$TMP_META_DIR/filterd"
+WISSBI_FILTER_PID_PREFIX="$TMP_META_DIR/filterd"
+
+WISSBI_META_DIR="$TMP_META_DIR"
+WISSBI_SUB_BINARY="$BUILD_DIR/wissbi-sub"
+WISSBI_PUB_BINARY="$BUILD_DIR/wissbi-pub"
+
+. $PROJECT_ROOT/scripts/wissbi_filter_template.sh
+
+EOF
+    chmod 755 $TMP_META_DIR/filterd
+    env WISSBI_META_DIR=$TMP_META_DIR $TMP_META_DIR/filterd start
+    sleep 2
+
+    echo "hello foo" | env WISSBI_META_DIR=$TMP_META_DIR $BUILD_DIR/wissbi-pub foo.in
+    sleep 1
+    $TMP_META_DIR/filterd stop
+
+    assertEquals "hello bar" "`cat $TMP_META_DIR/filterd-1-filter.out`"
+}
+
 testFilter()
 {
     cat > $TMP_META_DIR/filterd <<EOF
@@ -27,6 +57,39 @@ EOF
 
     echo "hello foo" | env WISSBI_META_DIR=$TMP_META_DIR $BUILD_DIR/wissbi-pub foo.in
     sleep 1
+    $TMP_META_DIR/filterd stop
+    kill $SUB_PID
+
+    assertEquals "hello bar" "`cat $TMP_META_DIR/received`"
+}
+
+testNoSub()
+{
+    cat > $TMP_META_DIR/filterd <<EOF
+#!/bin/sh
+
+WISSBI_FILTER_COUNT="1"
+WISSBI_FILTER_CMD="echo 'hello bar'"
+WISSBI_FILTER_SOURCE=""
+WISSBI_FILTER_SINK="foo.out"
+WISSBI_FILTER_LOG_PREFIX="$TMP_META_DIR/filterd"
+WISSBI_FILTER_PID_PREFIX="$TMP_META_DIR/filterd"
+
+WISSBI_META_DIR="$TMP_META_DIR"
+WISSBI_SUB_BINARY="$BUILD_DIR/wissbi-sub"
+WISSBI_PUB_BINARY="$BUILD_DIR/wissbi-pub"
+
+. $PROJECT_ROOT/scripts/wissbi_filter_template.sh
+
+EOF
+    chmod 755 $TMP_META_DIR/filterd
+    env WISSBI_META_DIR=$TMP_META_DIR $BUILD_DIR/wissbi-sub foo.out > $TMP_META_DIR/received &
+    SUB_PID=$!
+    env WISSBI_META_DIR=$TMP_META_DIR $TMP_META_DIR/filterd start
+    sleep 2
+
+    #echo "hello foo" | env WISSBI_META_DIR=$TMP_META_DIR $BUILD_DIR/wissbi-pub foo.in
+    #sleep 1
     $TMP_META_DIR/filterd stop
     kill $SUB_PID
 
