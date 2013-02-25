def7625f51a418c7c6c2503fb27706d205a62572
lunastorm <lunastorm@zillians.com>
2013-02-25 14:12:57 +0800

    add backup and restore function for metadata

--- scripts/wsbmetad.sh
+++ scripts/wsbmetad.sh
@@ -4,9 +4,12 @@ set -o nounset
 set -o errexit
 
 : ${WISSBI_META_DIR:="/var/lib/wissbi"}
+: ${WISSBI_BACKUP_DIR:="/var/lib/wissbi-backup"}
 
+rsync -rdlpAog $WISSBI_BACKUP_DIR/* $WISSBI_META_DIR
 while true
 do
+    rsync -rdlpAog $WISSBI_META_DIR/* $WISSBI_BACKUP_DIR
     for ENTRY in `find $WISSBI_META_DIR/sub | grep ":"`
     do
         ATIME=`stat -c "%X" $ENTRY || true`
