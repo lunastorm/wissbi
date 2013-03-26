35d718874451ad6ee1bb0432621e007f2c96a3c1
lunastorm <lunastorm@zillians.com>
2013-03-26 11:17:09 +0800

    remove unused field

--- src/msg_buf.hpp
+++ src/msg_buf.hpp
@@ -1,8 +1,6 @@
 #ifndef WISSBI_MSG_BUF_HPP_
 #define WISSBI_MSG_BUF_HPP_
 
-#include <string>
-
 namespace wissbi {
 
 const size_t MAX_MSG_SIZE = 4096;
@@ -11,7 +9,6 @@ typedef struct msg_buf {
     long mtype;
     char buf[MAX_MSG_SIZE];
     size_t len;
-    std::string dest;
 } MsgBuf;
 
 }
